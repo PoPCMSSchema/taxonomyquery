2e8d2512a74fbd64f51371c4b4a6135d62c53bef
Leonardo Losoviz <leo@getpop.org>
2020-08-08 16:52:51 +0800

    Include owner in depended migration plugin

--- src/Component.php
+++ src/Component.php
@@ -23,7 +23,7 @@ class Component extends AbstractComponent
     public static function getDependedMigrationPlugins(): array
     {
         return [
-            'migrate-taxonomyquery',
+            'pop-schema/migrate-taxonomyquery',
         ];
     }
 }
